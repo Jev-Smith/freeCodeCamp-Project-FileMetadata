cab25660a655b57d2a8edfeea2782d260d137318
Jevaughn Smith <smithjevaughn20@gmail.com>
2024-06-12 05:11:54 -0500

    Added required endpoint

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
 var app = express();
+
+app.use(express.urlencoded({ extended: false }));
+
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './files')
+  },
+  filename: (req, file, cb) => {
+    let date = new Date().toDateString();
+    cb(null, `${date}-${file.originalname}`)
+  }
+});
+
+const upload = multer({ storage });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +26,13 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.status(200).json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+})
 
 
 const port = process.env.PORT || 3000;
